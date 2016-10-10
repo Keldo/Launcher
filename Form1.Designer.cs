aceb95c256bccf7bc00f736357028f3fb0076b9c
Keldo <scott@twedev.com>
2016-10-10 16:31:47 -0700

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBarPictureBox = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label1.Location = new System.Drawing.Point(542, 403);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "WoW 7.0.3";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.DarkRed;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Image = global::Launcher.Properties.Resources.MinimizeButton;
+            this.button3.Location = new System.Drawing.Point(838, -1);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(23, 19);
+            this.button3.TabIndex = 5;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Launcher.Properties.Resources.WOW;
+            this.pictureBox1.Location = new System.Drawing.Point(36, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(760, 366);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkOrange;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Image = global::Launcher.Properties.Resources.Play_No_Hover;
+            this.button2.Location = new System.Drawing.Point(743, 389);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 35);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.DarkRed;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Image = global::Launcher.Properties.Resources.ExitButton;
+            this.button1.Location = new System.Drawing.Point(856, -1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(23, 19);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label2.Location = new System.Drawing.Point(12, 1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "World of Warcraft 7.0.3.22747";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.Black;
+            this.progressBar1.ForeColor = System.Drawing.Color.DarkOrange;
+            this.progressBar1.Location = new System.Drawing.Point(36, 406);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(469, 10);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 389);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // progressBarPictureBox
+            // 
+            this.progressBarPictureBox.Location = new System.Drawing.Point(36, 367);
+            this.progressBarPictureBox.Name = "progressBarPictureBox";
+            this.progressBarPictureBox.Size = new System.Drawing.Size(469, 19);
+            this.progressBarPictureBox.TabIndex = 9;
+            this.progressBarPictureBox.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label4.Location = new System.Drawing.Point(12, 427);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "label4";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Black;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.Color.DarkOrange;
+            this.button4.Location = new System.Drawing.Point(743, 389);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(109, 37);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Patch";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(878, 442);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBarPictureBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Trinity";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox progressBarPictureBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using Launcher.Properties;
+using System.Threading;
+
+namespace Launcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            checkPatch();
+            FileVersionInfo launcherVersion = FileVersionInfo.GetVersionInfo("Launcher.exe");
+            //var remoteVersionFile = "http://www.twedev.com/game/version";
+            //using (System.IO.TextReader rVF = new System.IO.StreamReader(remoteVersionFile));
+            //string remoteVersion = remoteVersionFile.ToString();
+            //string localVersion = launcherVersion.FileVersion;
+            //if (localVersion != remoteVersion)
+           // {
+             //label4.Text = "Launcher Update Available";
+            //}
+            //else
+           // {
+                //label4.Text = "Launcher Version: " + launcherVersion.FileVersion;
+           // }
+            label4.Text = "Launcher Version: " + launcherVersion.FileVersion;
+            button4.Visible = false;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("Wow_Patched.exe");
+
+            this.Close();
+        }
+
+        private void checkPatch()
+        {
+            // Check for Wow Patch
+            string patchexe = @"Wow_Patched.exe";
+            if (!File.Exists(patchexe))
+            {
+                label1.Text = "WOW PATCH IS NOT INSTALLED";
+                //button2.Image = Properties.Resources.PlayButtonDisabled;
+                progressBar1.Visible = false;
+                button4.Visible = true;
+                getPatch();
+            }
+            else
+            {
+                label1.Text = "World of Warcraft is up to date";
+                progressBar1.Visible = false;
+                button2.Image = Properties.Resources.Play_No_Hover;
+            }
+        }
+
+        private void getPatch()
+        {
+            var t = Task.Run(() => {
+                label1.Text = "Downloading Connection Patcher";
+                button2.Image = Properties.Resources.PlayButtonDisabled;
+                //button4.Visible = true;
+                progressBar1.Visible = true;
+                progressBar1.BackColor = Color.Black;
+                progressBar1.ForeColor = Color.DarkOrange;
+            
+                var webClient = new WebClient();
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                webClient.DownloadFileAsync(new Uri(Settings1.Default.connection_patcher), "connection_patcher.exe");
+            });
+            t.Wait();
+            GetM1();
+        }
+
+        private void GetM1()
+        {
+            var t = Task.Run(() => {
+                button2.Image = Properties.Resources.PlayButtonDisabled;
+                //button4.Visible = true;
+                label1.Text = "Downloading Readme PDF";
+                var webClient = new WebClient();
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                webClient.DownloadFileAsync(new Uri(Settings1.Default.readme_patcher), "PATCHER_README.pdf");
+            });
+            t.Wait();
+            GetM2();
+
+        }
+
+        private void GetM2()
+        {
+            var t = Task.Run(() => {
+                button2.Image = Properties.Resources.PlayButtonDisabled;
+                //button4.Visible = true;
+                label1.Text = "Downloading";
+                var webClient = new WebClient();
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                webClient.DownloadFileAsync(new Uri(Settings1.Default.libeay), "libeay32.dll");
+            });
+            t.Wait();
+            GetM3();
+        }
+
+        private void GetM3()
+        {
+            var t = Task.Run(() => {
+                button2.Image = Properties.Resources.PlayButtonDisabled;
+                //button4.Visible = true;
+                label1.Text = "Downloading";
+                var webClient = new WebClient();
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                webClient.DownloadFileAsync(new Uri(Settings1.Default.libmysql), "libmysql.dll");
+            });
+            t.Wait();
+            GetM4();
+        }
+
+        private void GetM4()
+        {
+            var t = Task.Run(() => {
+                button2.Image = Properties.Resources.PlayButtonDisabled;
+               // button4.Visible = true;
+                label1.Text = "Downloading";
+                var webClient = new WebClient();
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                webClient.DownloadFileAsync(new Uri(Settings1.Default.libssl), "libssl32.dll");
+            });
+            t.Wait();
+            GetM5();
+        }
+
+        private void GetM5()
+        {
+            var t = Task.Run(() => {
+                button2.Image = Properties.Resources.PlayButtonDisabled;
+            //button4.Visible = true;
+                label1.Text = "Downloading";
+                var webClient = new WebClient();
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                webClient.DownloadFileAsync(new Uri(Settings1.Default.ssleay), "ssley32.dll");
+                webClient.DownloadStringCompleted += OnGetDownloadedStringCompleted;
+            });
+            t.Wait();
+            GetM6();
+        }
+
+        private void GetM6()
+        {
+            var t = Task.Run(() => {
+                Directory.CreateDirectory("WTF");
+                 button2.Image = Properties.Resources.PlayButtonDisabled;
+            //button4.Visible = true;
+                label1.Text = "Downloading Config";
+                var webClient = new WebClient();
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                webClient.DownloadFileAsync(new Uri(Settings1.Default.config), "WTF\\Config.wtf");
+                webClient.DownloadStringCompleted += OnGetDownloadedStringCompleted;
+            });
+            t.Wait();
+            DownloadComplete();
+        }
+
+        private void DownloadComplete()
+        {
+            var t = Task.Run(() => {
+                label1.Text = "Downloads Complete.";
+                progressBar1.Visible = false;
+                button4.Visible = true;
+                //button2.Visible = true;
+            });
+            t.Wait();
+            goPatch();
+        }
+
+        private void goPatch()
+        {
+            label1.Text = "Ready to Patch.";
+            progressBar1.Visible = false;
+            button4.Visible = true;
+        }
+        private void CloseLauncher()
+        {
+            MessageBox.Show(this, "When the Download is complete, close the Launcher and View the Patcher Readme", "NOTICE", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void OnGetDownloadedStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            label1.Text = "Download complete!";
+            //button2.Image = Properties.Resources.PlayButtonDisabled;
+            button4.Visible = true;
+            progressBar1.Visible = false;
+        }
+
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Visible = true;
+            progressBar1.Value = e.ProgressPercentage;
+            progressBar1.BackColor = Color.Black;
+            progressBar1.ForeColor = Color.DarkOrange;
+
+            button2.Image = Properties.Resources.PlayButtonDisabled;
+            //button4.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var t = Task.Run(() => {
+               Process.Start(@"connection_patcher.exe", @"Wow.exe"); 
+            });
+            t.Wait();
+
+            button4.Visible = false;
+            progressBar1.Visible = false;
+            button2.Image = Properties.Resources.Play_No_Hover;
+            label1.Text = "World of Warcraft is up to date";
+        }
+    }
+}
